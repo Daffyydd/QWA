8e5ce4c0143dac3eb051d8ce64c5c4c5304214c3
Daffyydd <dnyakundi@gmail.com>
2021-10-12 13:49:40 +0300

    2 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './database/in-memory-data.service'
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
