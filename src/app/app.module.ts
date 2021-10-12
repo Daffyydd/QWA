68dc61e408aa9a4b4a6fed6ccb0818d7ea2043e7
Daffyydd <dnyakundi@gmail.com>
2021-10-12 21:33:20 +0300

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import {InMemoryDataService} from './database/in-memory-data.service'
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,HttpClientModule,HttpClientInMemoryWebApiModule,HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService,{dataEncapsulation:false}) ],
-  declarations: [ AppComponent, HelloComponent,HeroesComponent ],
+  declarations: [ AppComponent, HelloComponent,HeroesComponent,HeroDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
