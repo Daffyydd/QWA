d6e13115d14a9cb272e87139cc446fb38d24ccc5
Daffyydd <dnyakundi@gmail.com>
2021-10-13 13:59:55 +0300

    1 file modified

--- src/app/services/security/auth.service.ts
+++ src/app/services/security/auth.service.ts
@@ -1,8 +1,13 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+const AUTH_API = 'api/auth/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable()
 export class AuthService {
-
-  constructor() { }
-
+  constructor() {}
 }
