f7ab125de7ec5cc5ad8d60570323e7a3ddf9f953
Daffyydd <dnyakundi@gmail.com>
2021-10-13 10:15:57 +0300

    2 files modified

--- src/app/database/in-memory-data.service.ts
+++ src/app/database/in-memory-data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from '../models/hero';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class InMemoryDataService implements InMemoryDbService {
     return {heroes};
   }
   constructor() { }
- // genId(heroes: Hero[]): number {
-  //  return heroes.length > 0 ? Math.max(...heroes.map(hero => //hero.id)) + 1 : 11;
- // }
+ genId(heroes: Hero[]): number {
+   return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
 }
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HEROES } from '../database/mock-heroes';
@@ -8,18 +9,24 @@ import { MessageService } from './message.service';
   providedIn: 'root',
 })
 export class HeroService {
-
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) {}
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
   getHero(id: number): Observable<Hero> {
     // For now, assume that a hero with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
-    const hero = HEROES.find(h => h.id === id)!;
+    const hero = HEROES.find((h) => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
   }
-} 
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+}
