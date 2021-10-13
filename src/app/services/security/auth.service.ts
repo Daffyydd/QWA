855c88d33eeebbfc05bd2b822db1c6a51be6621f
Daffyydd <dnyakundi@gmail.com>
2021-10-13 14:10:33 +0300

    1 file modified

--- src/app/services/security/auth.service.ts
+++ src/app/services/security/auth.service.ts
@@ -1,13 +1,30 @@
-import { HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 const AUTH_API = 'api/auth/';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class AuthService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'login',
+      { username, password },
+      httpOptions
+    );
+  }
+
+  register(username: string, email: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'signup',
+      { username, email, password },
+      httpOptions
+    );
+  }
 }
